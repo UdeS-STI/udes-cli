9ec536e4a5ce121773a42dd23f1b75962130e9f9
Benjamin Rancourt <benjamin.rancourt@usherbrooke.ca>
2017-11-28 08:53:53 -0500

    Ajout de HTML (#29)

--- src/lint/test/FormatTest.js
+++ src/lint/test/FormatTest.js
@@ -9,7 +9,7 @@ describe('Format', () => {
     it('should set correct bash commands', () => {
       const lint = new Format({ path: '.' })
       expect(lint.commands.html()).to.be.equal('eslint . --ext html --ignore-path .gitignore --fix')
-      expect(lint.commands.js()).to.be.equal('eslint . --ext js,json --ignore-path .gitignore --fix')
+      expect(lint.commands.js()).to.be.equal('eslint . --ext html,js,json --ignore-path .gitignore --fix')
       expect(lint.commands.polymer).to.be.equal('polymer lint --fix')
     })
   })
