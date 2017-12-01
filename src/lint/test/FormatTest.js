c718415bd9a2186a7d142d51200c9710162db3f8
Benjamin Rancourt <benjamin.rancourt@usherbrooke.ca>
2017-12-01 15:18:37 -0500

    Mise a jour de @udes/shelljs-nodecli (#34)

--- src/lint/test/FormatTest.js
+++ src/lint/test/FormatTest.js
@@ -10,7 +10,7 @@ describe('Format', () => {
       const lint = new Format({ path: '.' })
       expect(lint.commands.html()).to.be.equal('eslint . --ext html --ignore-path .gitignore --fix')
       expect(lint.commands.js()).to.be.equal('eslint . --ext html,js,json --ignore-path .gitignore --fix')
-      expect(lint.commands.polymer).to.be.equal('polymer-cli lint --fix')
+      expect(lint.commands.polymer).to.be.equal('polymer lint --fix')
     })
   })
 })
